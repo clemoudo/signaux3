7e4443ac6546e6f793e8f4f2b9958d87c568c27a
Corentin Mertens <c.mertens@students.ephec.be>
2025-11-29 18:24:49 +0100

    Update detecteur.py
    updata pour integrer le ground check

--- detecteur.py
+++ detecteur.py
@@ -2,14 +2,20 @@ import cv2
 import numpy as np
 import time
 import classes
-from numpy.ma.extras import average
+import matplotlib.pyplot as plt
+import sounddevice as sd
+import soundfile as sf
 
+data, fs = sf.read("son.mp3", dtype="float32")
+
+donnes = []
+temps = []
 cv2.namedWindow("preview")
 
 vc = cv2.VideoCapture(0)
 
 person = []
-
+potential_fall_frames = []
 xmid = 0
 ymid = 0
 show_box = False
@@ -36,9 +42,12 @@ start_time = time.time()
 frame_idx = 1
 
 check_frame = ((0,500),(0,500))
-
+fall_confirmed = False
 last_bed_frame = None
-
+last_uner_bed_frame = None
+motion_traeshold = 0.0005
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')   # ou 'XVID' pour .avi
+out = cv2.VideoWriter('sortie.mp4', fourcc, fps, (target_w, target_h))
 while True:
     rval, frame = vc.read()
     if not rval:
@@ -54,9 +63,9 @@ while True:
     kernel = np.ones((5,5),np.uint8)
     motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_OPEN, kernel)
 
-    if not VP.still:
+    if not VP.still and not VP.potential_fall:
         motion_mask_ratio = np.count_nonzero(motion_mask) / motion_mask.size
-    if counter % 10 != 0:
+    if counter % 20 != 0:
         counter += 1
     else:
         counter =0
@@ -67,9 +76,9 @@ while True:
             cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
             xmid = (x_min + x_max) // 2
             ymid = (y_min + y_max) // 2
-        if motion_mask_ratio > 0.0005:
+        if motion_mask_ratio > 0.0005 and not VP.potential_fall:
             VP.motion_frames.append(((x_min+x_max+y_min+y_max)/4,[x_min, y_min, x_max, y_max]))
-            if len(VP.motion_frames) > 20:
+            if len(VP.motion_frames) > 15:
                 VP.motion_frames.pop(0)
             VP.in_motion = True
         if VP.in_motion:
@@ -81,59 +90,109 @@ while True:
                 VP.still_counter = 0
             else:
                 VP.still_counter += 1
-            if VP.still_counter > 200:
+            if VP.still_counter > 100:
                 VP.in_motion = False
                 VP.still_counter = 0
                 VP.still = True
                 VP.change_frame(VP.estimate_bed_frame())
                 still_count = 0
                 last_bed_frame = frame[int(VP.box_frame['U']):int(VP.box_frame['D']),int(VP.box_frame['L']):int(VP.box_frame['R'])]
+                last_under_bed_frame = frame[int(VP.box_frame['D']):int(VP.box_frame['D']+(int(VP.box_frame['D']-int(VP.box_frame['U'])))),int(VP.box_frame['L']):int(VP.box_frame['R'])]
         elif VP.still:
             show_box = True
             if still_count % 5 == 0:
                 bed_frame = frame[int(VP.box_frame['U']):int(VP.box_frame['D']),int(VP.box_frame['L']):int(VP.box_frame['R'])]
                 bed_frame_ratio = abs(np.mean(bed_frame) - np.mean(last_bed_frame))
-                print(bed_frame_ratio)
-                ys, xs = np.where(motion_mask > 0)
-                if len(xs) > 0 and len(ys) > 0:
-                    x_min, x_max = xs.min(), xs.max()
-                    y_min, y_max = ys.min(), ys.max()
-                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
-                    xmid = (x_min + x_max) // 2
-                    ymid = (y_min + y_max) // 2
-                    print('bfm',bed_frame_ratio)
-                if bed_frame_ratio > 20:
+                under_bed_frame = frame[int(VP.box_frame['D']):int(VP.box_frame['D']+(int(VP.box_frame['D']-int(VP.box_frame['U'])))),int(VP.box_frame['L']):int(VP.box_frame['R'])]
+                under_bed_frame_ratio = abs(np.mean(under_bed_frame)-np.mean(last_under_bed_frame))
+                donnes.append(bed_frame_ratio)
+
+                t = frame_idx / fps
+                temps.append(t)
+                if bed_frame_ratio > 10 and under_bed_frame_ratio >15:
+                    ys, xs = np.where(motion_mask[int(VP.box_frame['U']):int(VP.box_frame['D']),int(VP.box_frame['L']):int(VP.box_frame['R'])] > 0)
+                    if len(xs) > 0 and len(ys) > 0:
+                        x_min, x_max = xs.min(), xs.max()
+                        y_min, y_max = ys.min(), ys.max()
+                        cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+                        xmid = (x_min + x_max) // 2
+                        ymid = (y_min + y_max) // 2
+                        print('bfm', bed_frame_ratio)
                     print('potentiel')
                     VP.still = False
-                    VP.potential_fall = True
-                    if VP.check_potential_fall((xmid, ymid)):
-                        print('chute_détectée')
-                        #print(sdx)
-                    else:
-                        VP.still = True
-                        VP.potential_fall = False
+                    if VP.check_movement((xmid, ymid)):
+                        if VP.check_potential_fall((xmid, ymid)):
+                            print('potentielle_chute_détectée')
+                            potential_fall_frames = []
+                            potential_fall_frames.append(motion_mask_ratio)
+                            VP.potential_fall = True
+                        else:
+                            print('reset')
+                            VP.potential_fall = False
                 still_count = 0
             else:
                 still_count += 1
+        elif VP.potential_fall:
+            print('top')
+            motion_mask_ratio = np.count_nonzero(motion_mask) / motion_mask.size
+            ys, xs = np.where(motion_mask > 0)
+            if len(xs) > 0 and len(ys) > 0:
+                x_min, x_max = xs.min(), xs.max()
+                y_min, y_max = ys.min(), ys.max()
+                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+                xmid = (x_min + x_max) // 2
+                ymid = (y_min + y_max) // 2
+            potential_fall_frames.append(motion_mask_ratio)
+            print(len(potential_fall_frames))
+            print(potential_fall_frames)
+            if len(potential_fall_frames) > 100:
+                potential_fall_frames = potential_fall_frames[40:]
+                mavg = 0
+                for f in potential_fall_frames:
+                    mavg += f
+                mavg = mavg / len(potential_fall_frames)
+                print('mavg ',mavg)
+                if mavg < 0.001:
+                    print('chute confirmée')
+                    plt.plot(temps, donnes)
+                    plt.xlabel('Temps (s)')
+                    plt.ylabel('bfm (bed_frame_ratio)')
+                    plt.title('Évolution de bfm en fonction du temps')
+                    plt.grid(True)
+                    plt.show()
+                    VP.still = False
+                    VP.potential_fall = False
+                    duree_sec = 3
+                    fall_display_frames = int(duree_sec * fps)
+                    fall_confirmed = True
+                else:
+                    VP.potential_fall = False
+                    donnes.append(0)
+                    t = frame_idx / fps
+                    temps.append(t)
 
-
-
-
-
-
-
-
-
-
-
-
+    if fall_confirmed and fall_display_frames > 0:
+        cv2.putText(
+            frame,
+            "CHUTE CONFIRMEE",
+            (50, 100),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.5,
+            (0, 0, 255),
+            3,
+            cv2.LINE_AA
+        )
+        fall_display_frames -= 1
+        if fall_display_frames <= 0 and fall_confirmed:
+            sd.play(data,fs)
+            fall_confirmed = False
 
     #check_img = frame[check_frame[0][0]:check_frame[0][1],check_frame[1][0]:check_frame[1][1]]
     cv2.circle(frame, VP.pos, 10, (255, 0, 255), -1)
     cv2.circle(frame, (xmid, ymid), 1, (0, 0, 255), -1)
     if show_box:
         cv2.rectangle(frame,(int(VP.box_frame["L"]),int(VP.box_frame["U"])),(int(VP.box_frame["R"]),int(VP.box_frame["D"])),(0,255,0),2)
-
+        cv2.rectangle(frame,(int(VP.box_frame["L"]),int(VP.box_frame["D"])),(int(VP.box_frame["R"]),int(VP.box_frame["D"])+(int(VP.box_frame["D"])-int(VP.box_frame["U"]))),(255,0,0),2)
     cv2.putText(
         frame,  # image cible (ta sous-image)
         str(VP.still_counter),  # texte à écrire
@@ -162,6 +221,16 @@ while True:
         break
 
     frame_idx += 1
+    out.write(frame)
 
+
+
+plt.plot(temps, donnes)
+plt.xlabel('Temps (s)')
+plt.ylabel('bfm (bed_frame_ratio)')
+plt.title('Évolution de bfm en fonction du temps')
+plt.grid(True)
+plt.show()
 vc.release()
+out.release()
 cv2.destroyAllWindows()
